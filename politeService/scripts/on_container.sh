4ac7f92ce0609f4280ffaff7eb5ff6ede5a4b91d
Giovanni Di Blasi <=>
2019-03-20 21:02:58 +0100

    added interacive bash script

--- politeService/scripts/on_container.sh
+++ politeService/scripts/on_container.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 user_cmd=$1
 start_cmd=start
+bash_cmd=bash
 install_cmd=install
+base_command="docker run --rm -v $(pwd):/app -w /app"
+base_image="node:8-alpine" 
+
 if [[ "$user_cmd" == "$start_cmd" ]]; then
-    docker run --rm -v "$(pwd)":/app -w /app -p 3001:3000 node:8-alpine sh -c 'node .'
+    $base_command -p 3001:3000 $base_image sh -c 'node .'
 elif [[ "$user_cmd" == "$install_cmd" ]]; then
-    docker run --rm -v "$(pwd)":/app -w /app node:8-alpine sh -c 'npm install'
+    $base_command $base_image sh -c 'npm install'
+elif [[ "$user_cmd" == "$bash_cmd" ]]; then
+   $base_command -ti -p 3001:3000 $base_image sh
 else 
-    echo "./scripts/on_container.sh [ install | start ]"
+    echo "./scripts/on_container.sh [ install | start | bash]"
 fi
