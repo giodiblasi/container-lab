f2d1b2273858e93ed8b82ee4bc2276f3c8575ef0
Giovanni Di Blasi <=>
2019-03-20 23:22:24 +0100

    fix script

--- scripts/on_container.sh
+++ scripts/on_container.sh
@@ -4,15 +4,15 @@ port_binding=$2
 start_cmd=start
 bash_cmd=bash
 install_cmd=install
-base_command="docker run --rm -v $(pwd):/app -w /app -p $port_binding"
-base_image="node:8-alpine" 
-
+base_image="node:8-alpine"
+base_command="docker run --rm -v $(pwd):/app -w /app"
+docker_cmd=$([ ! -z "$port_binding" ] && echo "$base_command -p $port_binding" || echo $base_command)
 if [[ "$user_cmd" == "$start_cmd" ]]; then
-    $base_command  $base_image sh -c 'node .'
+    $docker_cmd $base_image sh -c 'node .'
 elif [[ "$user_cmd" == "$install_cmd" ]]; then
-    $base_command $base_image sh -c 'npm install'
+    $docker_cmd $base_image sh -c 'npm install'
 elif [[ "$user_cmd" == "$bash_cmd" ]]; then
-   $base_command -ti $base_image sh
+   $docker_cmd -ti $base_image sh
 else 
     echo "./scripts/on_container.sh [exposedPort:internalPort] [ install | start | bash]"
 fi
