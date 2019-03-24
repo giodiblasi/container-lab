b1f9a974bd4c603c81bf4a04636bbe23ff3ff5d1
Giovanni Di Blasi <=>
2019-03-24 12:03:23 +0100

    service url env variables

--- mainApp/index.js
+++ mainApp/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 
-const getName = 'http://users:3000';
-const sayHello = 'http://polite:3000';
+const USERS_SERVICE_URL = process.env.USERS_SERVICE_URL;
+const POLITE_SERVICE_URL = process.env.POLITE_SERVICE_URL;
 
 app.get('/:userid/sayhello', function (req, res) {
-    axios.get(`${getName}/${req.params.userid}/name`)
-        .then(response=> axios.get(`${sayHello}/${response.data}/hello`))
+    axios.get(`${USERS_SERVICE_URL}${req.params.userid}/name`)
+        .then(response=> axios.get(`${POLITE_SERVICE_URL}${response.data}/hello`))
         .then(response=> res.send(response.data))
-        .catch(err=> res.send("Houston we have a problem"));
+        .catch(err => res.send("Houston we have a problem"));
 });
 
 app.listen(3000, function () {
